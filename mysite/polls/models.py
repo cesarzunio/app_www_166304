58a15189829609561607da790505965e3cd2cd72
cesarzunio <andrzejmarekpatalon@gmail.com>
2024-11-14 02:04:06 +0100

    nowe modele

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -27,3 +27,30 @@ class Person(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Stanowisko(models.Model):
+
+    nazwa = models.CharField(max_length=100, blank=False, null=False)
+    opis = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nazwa
+
+
+PLEC_CHOICES = [
+        ('K', 'Kobieta'),
+        ('M', 'Mężczyzna'),
+        ('I', 'Inne')
+    ]
+
+
+class Osoba(models.Model):
+
+    imie = models.CharField(max_length=100, blank=False, null=False)
+    nazwisko = models.CharField(max_length=100, blank=False, null=False)
+    plec = models.CharField(max_length=1, choices=PLEC_CHOICES, blank=False, null=False)
+    stanowisko = models.ForeignKey(Stanowisko, on_delete=models.CASCADE, blank=False, null=False)
+
+    def __str__(self):
+        return f"{self.imie} {self.nazwisko}"
