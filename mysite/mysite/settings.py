a3217bcd884e4c4fbdaaf4bdfff68182714e3675
cesarzunio <andrzejmarekpatalon@gmail.com>
2024-11-20 06:18:14 +0100

    tokeny

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -22,7 +22,15 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'polls',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
